8523ac7b5dfad454582dea0a8498319f0afb6a86
Matilde Simonini <matilde.simonini@gmail.com>
2022-02-09 15:38:05 +0100

    aggiunta immagine e riempito array con singole inconcine

--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -18,32 +19,65 @@ public class GameScreen extends JPanel {
 	Random random;   //viene utilizzato per determinare a caso dei colori
 	private BufferedImage img;
 	
+	public static final int SINGLE_IMG_WIDTH = 32; //pixel, larghezza immaginina
 	public static final int COLOR_LIMIT = 256;   //valore limite colori escluso
-
+	private ArrayList<BufferedImage> sprites = new ArrayList<>();
+	
 	//costruttore
 	public GameScreen(BufferedImage img) {
 		this.img = img;
+		
+		loadSprites();
+		
 		this.random = new Random();
 	}
 	
+	/**
+	 * metodo per caricare tutte le immaginine (i singoli quadratini) come singole immagini
+	 * ossia quelle che costituiscono lo sprite presente nelle res.
+	 */
+	private void loadSprites() {
+		//(le immaginine sono 10 per riga e 10 per colonna, di dim 32 pixel)
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 10; j++) {
+				//BufferedImage singleImg = img.getSubimage(i * SINGLE_IMG_WIDTH, j * SINGLE_IMG_WIDTH, SINGLE_IMG_WIDTH, SINGLE_IMG_WIDTH); 
+				sprites.add(img.getSubimage(i * SINGLE_IMG_WIDTH, j * SINGLE_IMG_WIDTH, SINGLE_IMG_WIDTH, SINGLE_IMG_WIDTH));
+			}
+		}
+		
+	}
+
 	/**
 	 * Metodo che consente di disegnare sul componente: si usano tutti i metodi di Graphics per disegnare.
 	 * 
 	 * Ricorda: questo metodo non viene mai chiamato da nessun costruttore e neanche direttamente
 	 * dal programmatore.
 	 * Viene chiamato "automaticamente" quando si ha il bisogno di ridisegnare la finestra.
+	 * 
+	 * In questo caso l’orco verde è nella seconda riga e in ulti,ma posizione.
+	 * Quindi con 9x32 si raggiunge l’angolo in alto a sinistra del quadratino 
+	 * che contiene l’orco e per quanto riguarda la coordinata y sarà 1x32.
+	 * Questi ragionamenti vanno fatti basandosi sulle proprietà dell'immagine.
 	 */
 	public void paintComponent(Graphics g) {
 		//super che consente di disegnare lo sfondo del componente
 		super.paintComponent(g);
 		
 		g.setColor(Color.BLACK);
-		drawGrid(g);	
+		//drawGrid(g);	
+		
+		//disegnare l'immagine importata
+		//g.drawImage(img, 0, 0, null);
+		
+		//disegnare un elemento dello sprite atlas
+		g.drawImage(sprites.get(19), 60, 60, null);
+		//g.drawImage(img.getSubimage(9*32, 32, SINGLE_IMG_WIDTH, SINGLE_IMG_WIDTH), 50, 50 , null);
+			
 	}
 	
 	/**
+	 * metodo che crea una griglia di colori casuali.
 	 * 
-	 * metodo che consente di creare una griglia di quadrati
 	 */
 	private void drawGrid(Graphics g) {
 		
